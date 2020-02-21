6a0f638eb351d3b936f8eb04c88b1a77f0b7dee6
kamal <kamal.kumar@cmlmediasoft.com>
2020-02-21 17:52:19 +0530

    added prjt

---
+++ SwiftBluetooth/SwiftBluetooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBluetooth
+//
+//  Created by kamal on 21/02/20.
+//  Copyright © 2020 kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
